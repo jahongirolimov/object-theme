9ad406a8b4608985505185bac0812e0d79e45bb1
JOHON-DEV <machoman4701@gmail.com>
2024-01-25 05:58:38 +0500

    object theme

---
+++ index.js
@@ -0,0 +1,9 @@
+// let arr=[7, 4 , 2 , 3 , 1 ,  4 , 5 ,7]
+// let = function getSingleArr() {
+    
+// }
+// let arr=[7, 4 , 2 , 3 , 1 ,  4 , 5]
+// let a=Math.max(...arr)
+// let b=Math.min(...arr)
+// console.log(a)
+// console.log(b)
